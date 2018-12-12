bb24332fa9df3aedf624362b3edb6658afc58cdb
Scott Schmidt <sjschmidt44@gmail.com>
2018-12-12 12:27:43 -0800

    adds lecture code for class13

---
+++ class-13-model-relationships/demos/weather_relationships/src/tests/test_routes.py
@@ -0,0 +1,70 @@
+class TestWeatherRoutes:
+    """
+    """
+    @classmethod
+    def setup_class(cls):
+        """
+        """
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        """
+        """
+        pass
+
+    def setup_method(self, method):
+        """
+        """
+        pass
+
+    def teardown_method(self, method):
+        """
+        """
+        pass
+
+    def test_tc1(self):
+        """
+        """
+        pass
+
+    def test_tc2(self):
+        """
+        """
+        pass
+
+
+class TestCategoryRoutes:
+    """
+    """
+    @classmethod
+    def setup_class(cls):
+        """
+        """
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        """
+        """
+        pass
+
+    def setup_method(self, method):
+        """
+        """
+        pass
+
+    def teardown_method(self, method):
+        """
+        """
+        pass
+
+    def test_tc1(self):
+        """
+        """
+        pass
+
+    def test_tc2(self):
+        """
+        """
+        pass
